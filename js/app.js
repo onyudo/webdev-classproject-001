f18f013195ecd55a6462ea609f6cfde1b1df8c23
“Martin <martin@forceandform.com>
2024-12-31 12:16:32 -0500

    Created an array of quotes to serve randomly that consist of quote and author propertries. Created 2 additional const variables that reference quote and author IDs from html document.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 //JavaScript
+
+const quotes = [
+    { quote: "Not until we're totally crushed do we show what we are made of.", author: "Bohumil Hrabal" },
+    { quote: "In the depth of winter, I finally learned that within me there lay an invincible summer.", author: "Albert Camus" },
+    { quote: "All we have to decide is what to do with the time that is given us.", author: "J.R.R. Tolkien" },
+    // add more quotes here
+];
+
+const quoteElement = document.getElementById("random-quote");
+const authorElement = document.getElementById("source");
