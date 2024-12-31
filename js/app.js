0587bfaf9b96410f4bc1f44bc3cc52995082afff
“Martin <martin@forceandform.com>
2024-12-31 12:27:05 -0500

    Created empty function getRandomQuote in app.js

--- js/app.js
+++ js/app.js
@@ -11,10 +11,13 @@ const quoteElement = document.getElementById("random-quote");
 const authorElement = document.getElementById("source");
 
 // function generate random quote
+function getRandomQuote() {
+    
     // randomly choose a quote from the array of quotes
     // assign the randomly chosen quote to another constant variable?
 
     // use innerText to output quote to HTML document
     // user innerText to output author to HTML document
+}
 
 // render the randomly chosen quote and author combo to the HTML document
