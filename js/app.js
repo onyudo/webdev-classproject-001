045a664305a6ebd2dfa2c2054a29eaebeaa720c4
“Martin <martin@forceandform.com>
2025-01-15 15:37:32 -0500

    Updated code in app.js to include usedIndexes to keep track of the index of quotes in the quote array and updated my cycleQuotes function so that all quotes will be played randomly before being repeated.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //JavaScript
+"use strict";
 
 const quotes = [
     { quote: "Not until we're totally crushed do we show what we are made of.", author: "Bohumil Hrabal" },
@@ -10,14 +11,11 @@ const quotes = [
 const quoteElement = document.getElementsByClassName("random-quote")[0]; // Access the first element of this class
 const authorElement = document.getElementsByClassName("source")[0]; // Access the first element of this class
 
-// render and cycle through the quotes and author combo to the HTML document
-// found this on Stack Overflow
-window.addEventListener('load', function() {
-    cycleQuotes(); // Display the first quote immediately
-    setInterval(cycleQuotes, 9000); // Rotate quotes every 9 seconds (9000 milliseconds)
-});
+// Array to store indices of used quotes
+let usedIndexes = []; 
 
 /*
+// This was my original code and starting point prior to creating the cycleQuotes function
 // function generate random quote
 // IS THIS FUNCTION EVEN NEEDED ANYMORE?
 // Seems redundant, so I commented it out
@@ -40,8 +38,17 @@ function getRandomQuote() {
 // Is there a way to have the quotes "play" for a short amount of time and rotate randomly? YES
 // Integrate the randomization from the getRandomQuote function above
 function cycleQuotes() {
-    // Generate a random index
-    const randomIndex = Math.floor(Math.random() * quotes.length);
+    // Check if all quotes have been shown
+    if (usedIndexes.length === quotes.length) {
+        usedIndexes = []; // Reset the usedIndexes array if all quotes have been shown
+    }
+
+    // Generate a random index that hasn't been used yet
+    // If the random index has been used, the do...while loop will generate another one until it finds a one that hasn't been used.
+    let randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * quotes.length);
+    } while (usedIndexes.includes(randomIndex)); // Keep generating a new random index until it's not used
 
     // Get the random quote
     const quote = quotes[randomIndex];
@@ -49,6 +56,10 @@ function cycleQuotes() {
     // Display the quote and author
     quoteElement.innerText = quote.quote;
     authorElement.innerText = quote.author;
+
+    // Add the selected index to the usedIndexes array
+    // Keep track of indices
+    usedIndexes.push(randomIndex);
 }
     
     /*
@@ -62,4 +73,11 @@ function cycleQuotes() {
         currentQuoteIndex = 0;
     }
 }
-*/    
+*/   
+
+// render and cycle through the quotes and author combo to the HTML document
+// found this on Stack Overflow
+window.addEventListener('load', function() {
+    cycleQuotes(); // Display the first quote immediately
+    setInterval(cycleQuotes, 9000); // Rotate quotes every 9 seconds (9000 milliseconds)
+});
